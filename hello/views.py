f6a72c3a0b5babda9392684b79dcf0fc5fb69983
JohnShee <billy055187@gmail.com>
2017-05-02 00:05:20 +0800

    take a nap

--- hello/views.py
+++ hello/views.py
@@ -34,3 +34,19 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
+def search(request):
+    # #return HttpResponse('Hello from Python!')
+    # return render(request, 'index.html')
+
+    url_msg = str(request.__dict__)
+    print(url_msg)
+    print(request)
+    hi = "hi"
+    lat, lng = 25.034415, 121.568924
+    googlemap_api = os.environ.get("GOOGLEMAP_API")
+    googlemap_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={_lat},{_lng}&key={api}". \
+        format(_lat=lat, _lng=lng, api=googlemap_api)
+    #r = requests.get(googlemap_url)
+    return HttpResponse("象山第一站讚讚讚！")
+    # return HttpResponse("<pre>{}<pre>".format(r.text))
+
