49edae68d8bc5bf5a8a972695af6622632268be3
ChaiHao Chang <ChaiHao Chang>
2017-05-02 18:15:43 +0800

    json encode issue

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,79 @@
+# -*- coding: utf8 -*-
+
 from django.test import TestCase
 
+import json
+import requests
+import re
+import urllib
+import gzip
+from django.http import JsonResponse
 # Create your tests here.
+try:
+    float("hi")
+except ValueError:
+    print("not a value!")
+
+lat, lng = 0, 0
+
+if not(-90<= lat <= 90 and -180 <= lng <= 180): # check lat/lng
+    print("error")
+else:
+    print("do stuff")
+
+def get_distance(loc1, loc2): # relative distance
+    return sum(abs(l[0]-l[1])**2 for l in zip(loc1, loc2)) # norm(X1, X2)**2
+
+def testJson(lat, lng):
+
+    taipei_sarea = ["士林區", "北投區", "內湖區", "文山區", "南港區", "中山區"
+        , "大安區", "信義區", "松山區", "萬華區", "中正區", "大同區"]
+    url = "http://data.taipei/youbike"
+
+    r = urllib.request.Request(url)
+    response = urllib.request.urlopen(r)
+
+    decompressed_data = gzip.decompress(response.read())
+    r = json.loads(decompressed_data.decode("utf8"))
+    print(r["retCode"])
+    if r["retCode"] != 1:
+        return json.dumps({"code": 3, "result": []})
+    valid_stations = []
+    for i in r["retVal"].values():
+        if i["sarea"] in taipei_sarea and int(i["sbi"]) > 0:
+            valid_stations.append([i["sna"], i["sbi"], get_distance((lat, lng), [float(i) for i in (i["lat"], i["lng"])])])
+    valid_stations = sorted(valid_stations, key=lambda x: x[-1])
+    top_two = valid_stations[:min(2, len(valid_stations))]
+
+    if len(top_two) == 0:
+        return json.dumps({"code": 1, "result": []})
+    else:
+        return json.dumps({"code": 0, "result": [
+                    {
+                        "station": s[0],
+                        "num_ubike": s[1]
+                    } for s in top_two
+                ]
+             } )
+
+
+
+
+def testGoogle(url):
+    r = requests.get(url)
+    taipei = re.compile("Taipei City")
+    for i in r.text.split('"'):
+        if taipei.match(i):
+            print('IN')
+            return
+    print("OUT")
+
+loc1 = [100, 200]
+loc2 = [0, 0]
+print(get_distance(loc1, loc2))
+google_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=25.034542,121.568946&key=AIzaSyCMIdTtbpDsl4VDMBRmEO4QgPBRpLB5n_c"
+u = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=ddb80380-f1b3-4f8e-8016-7ed9cba571d5"
+j = (testJson(25.082, 121.57))
+print(j)
+print(json.loads(j))
+
--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,65 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
+import json
 import requests
 import os
+import re
+import urllib
+import gzip
 
 from .models import Greeting
 
+#dirty function
+def in_taipei(lat, lng):
+    googlemap_key = os.environ.get("GOOGLEMAP_API")
+    googlemap_url = \
+        "https://maps.googleapis.com/maps/api/geocode/json?latlng={_lat},{_lng}&key={key}".\
+        format(_lat=lat, _lng=lng, key=googlemap_key)
+
+    r = requests.get(googlemap_url)
+    taipei = re.compile("Taipei City") # Prevent the empty result/ lots of ifelse
+    for i in r.text.split('"'):
+        if taipei.match(i):
+            return True
+    return False
+
+def get_distance(loc1, loc2): # relative distance
+    return sum(abs(l[0]-l[1])**2 for l in zip(loc1, loc2)) # norm(X1, X2)**2
+
+def get_result(lat, lng):
+    taipei_sarea = ["士林區", "北投區", "內湖區", "文山區", "南港區", "中山區"
+        , "大安區", "信義區", "松山區", "萬華區", "中正區", "大同區"]
+
+    url = "http://data.taipei/youbike"
+    r = urllib.request.Request(url)
+    response = urllib.request.urlopen(r)
+    decompressed_data = gzip.decompress(response.read())
+    r = json.loads(decompressed_data.decode("utf8"))
+
+    print(r["retCode"])
+    if r["retCode"] != 1:
+        return json.dumps({"code": 3, "result": []})
+    valid_stations = []
+    for i in r["retVal"].values():
+        if i["sarea"] in taipei_sarea and int(i["sbi"]) > 0:
+            valid_stations.append([i["sna"], i["sbi"], get_distance((lat, lng), [float(i) for i in (i["lat"], i["lng"])])])
+    valid_stations = sorted(valid_stations, key=lambda x: x[-1])
+    top_two = valid_stations[:min(2, len(valid_stations))]
+
+    if len(top_two) == 0:
+        return json.dumps({"code": 1, "result": []})
+    else:
+        print(top_two)
+        return json.dumps({"code": 0, "result": [
+                    {
+                        "station": s[0],
+                        "num_ubike": s[1]
+                    } for s in top_two
+                ]
+             }, ensure_ascii=False)
+
+    # keep the neras
 # Create your views here.
 def index(request):
     # #return HttpResponse('Hello from Python!')
@@ -35,18 +89,23 @@ def db(request):
     return render(request, 'db.html', {'greetings': greetings})
 
 def search(request):
-    # #return HttpResponse('Hello from Python!')
-    # return render(request, 'index.html')
 
-    url_msg = str(request.__dict__)
-    print(url_msg)
-    print(request)
-    hi = "hi"
-    lat, lng = 25.034415, 121.568924
-    googlemap_api = os.environ.get("GOOGLEMAP_API")
-    googlemap_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={_lat},{_lng}&key={api}". \
-        format(_lat=lat, _lng=lng, api=googlemap_api)
-    #r = requests.get(googlemap_url)
-    return HttpResponse("象山第一站讚讚讚！")
-    # return HttpResponse("<pre>{}<pre>".format(r.text))
+    if not all(tag in request.GET for tag in ("lat", "lng")):
+        print("invalid url")
+        return JsonResponse({"code": -2, "result": []})
+
+    try:
+        lat = float(request.GET["lat"])
+        lng = float(request.GET["lng"])
+    except ValueError:
+        return JsonResponse({"code": -1, "result": []}) # invalid input
+
+    if not(-90<= lat <= 90 and -180 <= lng <= 180):
+        return JsonResponse({"code": -1, "result": []}) # invalid latitude or longitude
+
+    if not in_taipei(lat, lng):
+        return JsonResponse({"code": -2, "result": []}) # out of taipei city
+
+    return HttpResponse(get_result(lat, lng), content_type='application/json', charset = 'cp950' )
+
 
